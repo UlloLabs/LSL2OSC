6f33b28f325b59ced59d8a826ed96a589ef36b68
jfrey <nomhad@gmail.com>
2022-03-18 19:54:47 +0100

    sending data to OSC

--- LSL2OSC.py
+++ LSL2OSC.py
@@ -8,18 +8,37 @@ sys.path.insert(0, os.path.join(
 from ContinuousReader import ContinuousReader
 
 import argparse, time
+from pythonosc import udp_client
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pipe data from LSL to OSC.")
     parser.add_argument("--pred", type = str, default = "", help = """A predicate to use to filter streams. E.g. "type='EEG'", "type='EEG' and name='BioSemi'", "(type='EEG' and name='BioSemi') or type='HR'". Note that that predicat is case-sensitive. Default: empty, record all streams. CAUTION: if the predicate is malformed (e.g. messing up parentheses, or invalid characters) it can crash the outlet!""")
+    parser.add_argument("--ip", default = "127.0.0.1",
+      help="The IP or address of the OSC server (default: 127.0.0.1).")
+    parser.add_argument("--port", type = int, default = 5005,
+      help="The port the OSC server is listening on (default: 5005).")
+    parser.add_argument("-v", "--verbose", action='store_true', help="Print more verbose information.")
     args = parser.parse_args()
 
+    print("Starting OSC client to address %s and port %d" % (args.ip, args.port))
+    osc_client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+    print("Now watching for streams...")
     cr = ContinuousReader(pred=args.pred, fetch_all=True)
 
     def show_data(data):
-        print(data)
+        """
+        Callback function for LSL samples, will get data tuples with (sample, timestamp, name, type, hostname, uid, sampling rate, data format).
+        Forward to OSC server.
+        """
+        sample = data[0]
+        stream_name = data[2]
+        stream_type = data[3]
+        osc_url = "/%s/%s" % (stream_type, stream_name)
+        if args.verbose:
+            print(osc_url, sample)
+        osc_client.send_message(osc_url, sample)
 
-    print("Now watching for streams...")
     try:
         while True:
             cr.callmeback(show_data, pull_last=True)
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
-Retrieve data *from* Labstreaminglayer and forward it *to* an OSC server.
+Retrieve data *from* Labstreaminglayer and forward it *to* an (UDP) OSC server.
 
 If you wish to do the opposite (get data from OSC and pipe it through LSL), have a look at https://github.com/gisogrimm/osc2lsl
+
+As is, once connected to server, LSL data will be forwarded to addresses with format "/lsl_type/lsl_name".
 
 Because it would not make sense anyway with OSC when controlling sound or visuals to get many samples in a row, here for each loop (roughly 100hz) we always get last sample from each stream. Hack that or prefer another approach if you need all values, as OSC in sot suited for that.
 
